7fa376d21d61922753522b6e0f1eab5818100ade
oleg425top <oleg59152@gmail.com>
2024-11-25 15:56:00 +0500

    добавлен файл Furniture.py c самостоятельной работой

---
+++ Furniture.py
@@ -0,0 +1,21 @@
+class Furniture:
+    def __init__(self, type, material, prise):
+        self.type = type
+        self.material = material
+        self.prise = prise
+
+
+    def __str__(self):
+        return f'Это {self.type} из материала {self.material}, цена изделия {self.prise} рублей'
+
+
+    def buy(self):
+        return f'вы купили {self.type} за {self.prise} рублей'
+
+    def diskount(self, diskount):
+        self.prise -= (self.prise / 100) * diskount
+        return f'вы купили {self.type} за {self.prise} рублей cо скидкой в {diskount} процентов'
+
+chair = Furniture('стул', 'дсп', 1500)
+print(chair)
+print(chair.diskount(15))
