0b78e0ef417514c32d2dde4a0b49193f783036b4
oleg425top <oleg59152@gmail.com>
2024-11-25 15:24:27 +0500

    перенес экземпляры класса в main

--- Vehicle.py
+++ Vehicle.py
@@ -30,23 +30,5 @@ class Vehicle:
 
 
 
-BMW = Vehicle('BMW',150000)
-Audi = Vehicle('Audi',200000)
-Suzuki = Vehicle('Suzuki',50000)
-SkyBord = Vehicle('SkyBord',30000)
-print(Audi)
-print(Audi.get_vehicle_type(4))
-print(Audi.get_vehicle_advice())
-print(BMW)
-print(BMW.get_vehicle_type(4))
-print(BMW.get_vehicle_advice())
-print(Suzuki)
-print(Suzuki.get_vehicle_type(2))
-print(Suzuki.get_vehicle_advice())
-print(SkyBord)
-print(SkyBord.get_vehicle_type(3))
-print(SkyBord.get_vehicle_advice())
-
-
 
 
